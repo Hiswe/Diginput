976810eab3985a116750f1bdc0c5465defb1adb8
Hiswe <hiswehalya@gmail.com>
2010-10-06 22:12:08 +0200

    Start the refactor, add a getValue Method

--- jquery.ui.diginput.js
+++ jquery.ui.diginput.js
@@ -11,7 +11,8 @@
  */
 (function($, undefined){
 	$.widget("ui.diginput",{
-		options: {
+		options:
+        {
 			bindNameSpace : 'diginput',
     		idSuffix: '-copy',
 			classInput: 'diginput',
@@ -20,57 +21,54 @@
 			separator: 'fr',
 			x1000Button : '<a href="#" class="bouton boutonPetit"><span>x1000</span></a>',
 			onComplete : $.noop,
-			debug : false
+			debug :true
 		},
-		_create: function(){
-		        var o = this.options;
-				var $button = null ;
-				o.separator = checkSeparator(o.separator);
-				this.$copy = this._makeCopy();
-			var presentationData = this.element.val();
+		_create: function()
+        {
+            var $button = null ;
+            this.options.separator = checkSeparator(this.options.separator);
+            this.$copy = this._makeCopy();
 
-				this.$copy.bind('keyup.'+o.bindNameSpace,$.proxy(this._keyUp, this))
-				.bind('keypress.'+o.bindNameSpace, $.proxy(function(event){ // Keycontrol
-					if(!this._validKey(event)){
-						event.preventDefault();
-					}
-				}, this)).bind('change', $.proxy(function(event){
-                    this.element.trigger('change');
-                    (o.debug) ? console.info('[Event] Change | original Input') : null;
-                },this));
+            this.$copy
+                .bind('keyup.'+this.options.bindNameSpace,$.proxy(this._keyUp, this))
+                // Keycontrol
+				.bind('keypress.'+this.options.bindNameSpace, $.proxy(this._validKey, this))
+                .bind('change', $.proxy(this._copyChangeEvent,this));
 			// add x1000 button
-			if (o.x1000Button != ''){
-				this.$button = $(o.x1000Button);
+			if (this.options.x1000Button != ''){
+				this.$button = $(this.options.x1000Button);
 				this.$copy.after(this.$button);
-				this.$button.bind('click.'+o.bindNameSpace,$.proxy(this._btnClick, this));
+				this.$button.bind('click.'+this.options.bindNameSpace,$.proxy(this._x1000, this));
 			}
 		},
-        _keyUp : function(){
+        _copyChangeEvent: function(event)
+        {
+            this.element.trigger('change');
+            (this.options.debug) ? console.info('[Event] Change | original Input') : null;
+        },
+        _keyUp : function()
+        {
 			clearTimeout(timeOut);
-			var self = this;
+            var self = this;
 			var timeOut = setTimeout(function(){
 			// don't update if the values remain the same
 			var newData = self.$copy.val();
 				if (presentationData != newData){
+                    if(self.options.debug) console.log('[DIGINPUT] evnt :: keyUp with diffrent data');
 					presentationData = newData;
-					self._updateInput(this.format(newData, self.options.separator, false));
+					self._updateInput(self._innerFormat(newData, self.options.separator, false));
 				}
 				// TODO callBack
 			},200);
 		},
-        _btnClick : function(event){
-            this._x1000(event);
-            this.element.trigger('change');
-            (o.debug) ? console.info('[Event] Change | original Input') : null;
-
-         },
 		_makeCopy: function(){ // copy the input and hide the original
 			var $copy = this.element.clone();
             var newId = this.element.attr('id')+this.options.idSuffix;
+            this.value = this._innerFormat($copy.val());
 
 			$copy.addClass(this.options.classInput)
 				.attr('id', newId)
-                .val($.ui.diginput.format($copy.val(), this.options.separator, false).string)
+                .val(this.value.string)
 				.insertBefore(this.element);
 			// Change the label
 			var $label = $('label[for='+this.element.attr('id')+']');
@@ -81,32 +79,35 @@
 			(this.options.debug) ? this.element.fadeTo('normal', 0.5) : this.element.hide();
 			return $copy;
 		},
-		_x1000: function(event){
+		_x1000: function(event)
+        {
 			event.preventDefault();
 			event.stopPropagation();
+            this.element.trigger('change');
+            (this.options.debug) ? console.info('[Event] Change | original Input') : null;
 			var rawData = this.element.val();
 			if (!rawData){
 				rawData = 1;
 			}
 			var multipliedData = rawData * 1000;
 			if (multipliedData.toString().indexOf('e') == -1){
-				this._updateInput($.ui.diginput.format(multipliedData, this.options.separator, false));
+				this._updateInput(this._innerFormat(multipliedData));
 			}
 			// TODO callBack
 		},
-		_updateInput: function(newData){ // Update the inputs
+		_updateInput: function(newData)
+        { // Update the inputs
+                this.value = newData;
 				this.$copy.val(newData.string);
 			    this.element.val(newData.number);
 		},
-		_validKey: function(event){  // Check if a key is valid
-			var separator= this.options.separator;
+		_validKey: function(event) // Check if a key is valid
+        {
+            var separator = this.options.separator;
 			var isValid = true;
 			var authorizedCharacter = '1234657890'+separator.floatFix+separator.floatSep;
 			// convertit le charCode en ce qu'il recouvre
-			var key;
-			if (!event.charCode) key = String.fromCharCode(event.which);
-				else key = String.fromCharCode(event.charCode);
-
+			var key	=  (!event.charCode) ?  String.fromCharCode(event.which) : String.fromCharCode(event.charCode);
 			// authorized key
 			if (authorizedCharacter.indexOf(key) === -1) isValid = false;
 			// key combo
@@ -116,9 +117,10 @@
 			});
 			// delete test
 			if (event.which=='0' || event.which=='8') isValid = true;
-			return isValid;
+            if (!isValid) event.preventDefault();
 		},
-		destroy: function(){
+		destroy: function()
+        {
 			this.element.fadeTo('slow', 1);
 			this.$copy.fadeOut('fast',function(){$(this).remove()});
 			if (this.options.x1000Button != ''){
@@ -126,14 +128,16 @@
 			}
 			$.Widget.prototype.destroy.call( this );
 		},
-		disable: function(){
+		disable: function()
+        {
             var o = this.options;
 			this.$copy.attr('disabled','disabeld').fadeTo('normal',0.85, function(){$(this).addClass(o.disabledClass)});
 			if(this.$button){
 				this.$button.addClass(o.disabledClass).unbind('click.'+o.bindNameSpace).fadeTo('normal',0.5);
 			}
 		},
-		enable: function(){
+		enable: function()
+        {
             var o = this.options;
 			this.$copy.removeClass(o.disabledClass).attr('disabled','').fadeTo('normal',1);
 			if(this.$button){
@@ -142,20 +146,31 @@
 				.fadeTo('normal',1);
 			}
 		},
+        getVal: function()
+        {
+        },
+        setVal: function(value)
+        {
+            this._updateInput(this._innerFormat(value));
+        },
+        _innerFormat: function()
+        {
+            return $.ui.diginput.format(value, this.options.separator, false);
+        }
 		_setOption: function(key, value){
 			$.Widget.prototype._setOption.apply( this, arguments );
 			switch(key){
 				case "val":
 					if (value) {
-						this._updateInput($.ui.diginput.format(value, this.options.separator, false));
+						this._updateInput(this._innerFformat(value));
 					}
 				break;
 			}
-
 		}
 	});
 	/* Integer format with thousand separator */
-	function formatInteger(integer, thousandSeparator){
+	function formatInteger(integer, thousandSeparator)
+    {
 		integer = integer.replace(/\B(?=(\d\d\d)*$)/g,thousandSeparator);
 		// firefox need it, not chrome... duno why
 		if(integer.charAt(integer.length-1) == thousandSeparator){
@@ -164,7 +179,8 @@
 		return integer;
 	}
 	/* verify settings */
-	function checkSeparator(settings){
+	function checkSeparator(settings)
+    {
 			var template = {
 				frSetting: {
 					floatSep: '.',
@@ -215,99 +231,6 @@
 		return settings;
 	}
 	/* Thousand separator */
-	this.format = function(primaryData, separator, control) {
-		var regExpression;
-		var reg;
-		var settings;
-		var cleanData = null; /* common float */
-        var cleanDataNumber = null; /* clean number */
-		var cleanDataFormat = null; /* formated number */
-		/*Data verificiation*/
-		try
-		{
-			if (typeof primaryData == 'string'){
-			}
-			else if (typeof primaryData == 'number'){
-				cleanData = primaryData.toString();
-			}
-			else if (primaryData == undefined){
-				cleanData = '0';
-			    cleanDataFormat = '0';
-			}
-			else{
-				throw 'dataTypeError';
-			}
-		}
-		catch(err){
-			if (err == 'dataTypeError'){
-				debug('Data type Error in this.format()\n Data was : '+primaryData);
-				return;
-			}
-		}
-		// no seperator char defined -> default
-		settings = separator;
-		if (separator == undefined){
-			settings = checkSeparator($.ui.diginput.prototype.options.separator);
-		}
-		if(control == undefined && separator != undefined){
-            // seperator char not already defined (direct use of the public method)
-			settings = checkSeparator(separator);
-            if (typeof separator == 'string'){
-                settings = checkSeparator(separator);
-            }else{
-                settings = checkSeparator($.extend({},$.ui.diginput.prototype.options.separator, separator));
-            }
-		}
-		/* string behavior */
-		if (typeof primaryData =='string'){
-			// remove all but digits and float separators
-			regExpression = "[^[0-9"+settings.floatSep+settings.floatFix+"]]*";
-			reg = new RegExp(regExpression, "g");
-			cleanData = primaryData.replace(reg, '');
-			// Behavior when it's a float number
-			regExpression = "(["+settings.floatSep+settings.floatFix+"]+)";
-			reg = new RegExp(regExpression, "g");
-			if (reg.test(primaryData)){
-				// replace all floatFix by floatSep
-				if (settings.floatFix != ''){
-					regExpression = "["+settings.floatFix+"]"; // verification of the regExpression for IE browser. IE don't tolerate '[]' regexp.
-					reg = new RegExp(regExpression, "g");
-					cleanData = cleanData.replace(reg, settings.floatSep);
-				}
-				// clean the integer part of the number
-				regExpression = "^([0-9]*)("+settings.floatSep+")(.*)$";
-				reg = new RegExp(regExpression, "g");
-				var	cleanDataInteger = cleanData.replace(reg,'$1');
-				// capture the float part of the number
-				var cleanDataFloat = '';
-				regExpression = "^([0-9]*)("+settings.floatSep+")(.*)$";
-				reg = new RegExp(regExpression, "g");
-				cleanDataFloat = cleanData.replace(reg,'$3');
-				// clean the float part of the number
-				regExpression = "[^[0-9]]*";
-				reg = new RegExp(regExpression, "g");
-				cleanDataFloat = cleanDataFloat.replace(reg,'');
-				cleanData = cleanDataInteger+'.'+cleanDataFloat;
-			}
-		}
-		// integer format
-        if(cleanData == '.'){
-             cleanDataFormat = '';
-             cleanData = '';
-        }
-		else if (cleanData.indexOf('.') == -1){
-			cleanDataFormat = formatInteger(cleanData,settings.spacing);
-		}else{
-			var cleanDataSplit = cleanData.split('.');
-            cleanDataSplit[0] = (cleanDataSplit[0]) ? formatInteger(cleanDataSplit[0],settings.spacing) : 0;
-			cleanDataFormat = cleanDataSplit[0]+settings.floatSep+cleanDataSplit[1];
-		}
-        cleanDataNumber = (cleanData=='') ? '' : parseFloat(cleanData);
-    	return {    number : cleanDataNumber,
-			        string : cleanDataFormat
-		};
-	};
-
     $.extend( $.ui.diginput, {
 	    format: function( primaryData, separator, control) {
                 var regExpression;
